3da0a38f7745f799e91a2bd0bed7b3b200c81b1e
Arun  Kumar <161482514+Arun-Kumar-Mandleshwar@users.noreply.github.com>
2025-08-03 23:16:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function firstNonRepeatedChar(str) {
     for (let i = 0; i < str.length - 1; i++)
         for (let j = i; j < str.length; j++) {
             if (str[i] == str[j]) {
-                return str[i];
+                return "'"+str[i]+"'";
             }
         }
     return null;
