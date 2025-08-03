011c73eb7b3be53788d3bdb558f7b247e2e099d6
Arun  Kumar <161482514+Arun-Kumar-Mandleshwar@users.noreply.github.com>
2025-08-03 22:59:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
+
 function firstNonRepeatedChar(str) {
- // Write your code here
+    // Write your code here
+    let repeated = null;
+    let str1 = str.split('').sort();
+    console.log(str1);
+    for(let index in str1){
+        // console.log(index,str1[index],index+1,str1[index+1]);
+        if (str1[Number(index)]===str1[Number(index)+1])
+        {
+            repeated=str1[Number(index)];
+            break;
+        }
+    }
+ 
+    return repeated;
 }
+
 const input = prompt("Enter a string");
+
 alert(firstNonRepeatedChar(input)); 
